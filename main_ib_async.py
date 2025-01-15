1e6b18c5945b32481975780bc88c222fcd143595
changxingzhao <changxingzhao@bytedance.com>
2025-01-15 19:18:03 +0800

    Update IB connection parameters and enhance data retrieval in main_ib_async.py. Changed connection port to 4002, added historical data request for AAPL stock, and enabled DataFrame conversion for better data handling.

--- main_ib_async.py
+++ main_ib_async.py
@@ -2,7 +2,8 @@ from ib_async import *
 # util.startLoop()  # uncomment this line when in a notebook
 
 ib = IB()
-ib.connect('127.0.0.1', 4001, clientId = 1)
+# ib.connect('127.0.0.1', 4001, clientId = 1)
+ib.connect('127.0.0.1', 4002, clientId = 1)
 
 # ib.reqMarketDataType(4)  # Use free, delayed, frozen data
 # contract = Forex('EURUSD')
@@ -10,9 +11,15 @@ ib.connect('127.0.0.1', 4001, clientId = 1)
 #     contract, endDateTime='', durationStr='30 D',
 #     barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
 
-positions = ib.reqPositions()
-print(positions)
+ib.reqMarketDataType(4)  # Use free, delayed, frozen data
+contract = Stock('AAPL', 'SMART', 'USD')
+bars = ib.reqHistoricalData(
+    contract, endDateTime='', durationStr='30 D',
+    barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
 
 # convert to pandas dataframe (pandas needs to be installed):
-# df = util.df(bars)
-# print(df)
+df = util.df(bars)
+print(df)
+
+# positions = ib.reqPositions()
+# print(positions)
