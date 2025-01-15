4171a52a85b6083a6d172bdfc0a521f6cdcbd5ca
lucaschang95 <xingzhao.chang@gmail.com>
2025-01-15 22:47:00 +0800

    Add .gitignore and enhance data handling in main_ib_async.py
    - Introduced .gitignore to exclude all files in the data directory.
    - Updated historical data request for AAPL stock to a 3-day duration with 10-minute intervals.
    - Added print statements to display the type and details of the retrieved bars.
    - Commented out previous DataFrame conversion and CSV saving code for future reference.

--- main_ib_async.py
+++ main_ib_async.py
@@ -1,25 +1,61 @@
 from ib_async import *
+import pandas as pd
 # util.startLoop()  # uncomment this line when in a notebook
 
 ib = IB()
-# ib.connect('127.0.0.1', 4001, clientId = 1)
 ib.connect('127.0.0.1', 4002, clientId = 1)
 
+##################
 # ib.reqMarketDataType(4)  # Use free, delayed, frozen data
 # contract = Forex('EURUSD')
 # bars = ib.reqHistoricalData(
 #     contract, endDateTime='', durationStr='30 D',
 #     barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
+##################
 
+##################
 ib.reqMarketDataType(4)  # Use free, delayed, frozen data
 contract = Stock('AAPL', 'SMART', 'USD')
 bars = ib.reqHistoricalData(
-    contract, endDateTime='', durationStr='30 D',
-    barSizeSetting='1 hour', whatToShow='TRADES', useRTH=True)
+    contract, endDateTime='', durationStr='3 D',
+    barSizeSetting='10 mins', whatToShow='TRADES', useRTH=True)
+##################
 
+##################
 # convert to pandas dataframe (pandas needs to be installed):
-df = util.df(bars)
-print(df)
+# df = util.df(bars)
+# print(df)
+# Save bars data to local file
+##################
 
-positions = ib.reqPositions()
-print(positions)
+##################
+# # Save to csv file
+# filename = 'aapl_bars.csv'
+# df2 = util.df(bars)  # Convert to dataframe
+# df2.to_csv(filename)
+##################
+
+##################
+# df3 = pd.DataFrame(bars)
+# filename = 'aapl_bars3.csv'
+# df3.to_csv(filename)
+##################
+
+
+##################
+print("bars的类型:", type(bars))
+print("单个bar的类型:", type(bars[0]))
+print("\n第一个bar的详细信息:")
+print(vars(bars[0]))
+##################
+
+##################
+# filename = 'aapl_bars4.txt'
+# with open(filename, 'w') as f:
+#     for bar in bars:
+#         f.write(f'{bar}\n')
+##################
+
+
+# positions = ib.reqPositions()
+# print(positions)
