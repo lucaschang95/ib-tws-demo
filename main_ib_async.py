63eacec0e60048ef3aa572022a05f00789a01b3b
changxingzhao <changxingzhao@bytedance.com>
2025-01-16 14:31:20 +0800

    Refactor historical data retrieval in main_ib_async.py
    - Updated IB connection parameters to use port 4001.
    - Introduced a new function `get_historical_data` to streamline the retrieval of historical data for AAPL stock.
    - Enhanced data retrieval to support multiple batches with 1-minute intervals over a 3-day duration.
    - Added functionality to save retrieved data to a CSV file.
    - Removed outdated commented code for clarity.

--- main_ib_async.py
+++ main_ib_async.py
@@ -1,61 +1,45 @@
 from ib_async import *
 import pandas as pd
-# util.startLoop()  # uncomment this line when in a notebook
 
 ib = IB()
-ib.connect('127.0.0.1', 4002, clientId = 1)
-
-##################
-# ib.reqMarketDataType(4)  # Use free, delayed, frozen data
-# contract = Forex('EURUSD')
-# bars = ib.reqHistoricalData(
-#     contract, endDateTime='', durationStr='30 D',
-#     barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
-##################
-
-##################
+ib.connect('127.0.0.1', 4001, clientId = 1)
 ib.reqMarketDataType(4)  # Use free, delayed, frozen data
 contract = Stock('AAPL', 'SMART', 'USD')
-bars = ib.reqHistoricalData(
-    contract, endDateTime='', durationStr='3 D',
-    barSizeSetting='10 mins', whatToShow='TRADES', useRTH=True)
-##################
 
-##################
-# convert to pandas dataframe (pandas needs to be installed):
-# df = util.df(bars)
-# print(df)
-# Save bars data to local file
-##################
+def get_historical_data(ib, contract, filename='aapl_bars5.csv', num_batches=3):
+    end_date = ''  # Start with current time
+    temp_bars = []  # Temporary list to store batches
 
-##################
-# # Save to csv file
-# filename = 'aapl_bars.csv'
-# df2 = util.df(bars)  # Convert to dataframe
-# df2.to_csv(filename)
-##################
+    for i in range(num_batches):  # Get multiple sets of data
+        print(f'Requesting historical data batch {i+1}...')
+        bars = ib.reqHistoricalData(
+            contract, endDateTime=end_date, durationStr='3 D',
+            barSizeSetting='1 min', whatToShow='TRADES', useRTH=True)
+        print(f'Batch {i+1} received.')
+        
+        # Add the new bars to our temporary list
+        temp_bars.append(bars)
+        
+        # Update end_date to get next batch of historical data
+        if bars and len(bars) > 0:
+            # Use the earliest timestamp from current batch as next end time
+            end_date = bars[0].date.strftime('%Y%m%d %H:%M:%S')
+        
+        # Sleep briefly to avoid overwhelming the server
+        ib.sleep(5)
 
-##################
-# df3 = pd.DataFrame(bars)
-# filename = 'aapl_bars3.csv'
-# df3.to_csv(filename)
-##################
+    # Reverse the order and combine all bars
+    all_bars = []
+    for batch in reversed(temp_bars):
+        all_bars.extend(batch)
 
+    # Convert all collected bars to a single dataframe
+    bars = all_bars
 
-##################
-print("bars的类型:", type(bars))
-print("单个bar的类型:", type(bars[0]))
-print("\n第一个bar的详细信息:")
-print(vars(bars[0]))
-##################
+    # Save to csv file
+    df2 = util.df(bars)  # Convert to dataframe
+    df2.to_csv(filename)
+    
+    return bars
 
-##################
-# filename = 'aapl_bars4.txt'
-# with open(filename, 'w') as f:
-#     for bar in bars:
-#         f.write(f'{bar}\n')
-##################
-
-
-# positions = ib.reqPositions()
-# print(positions)
+# get_historical_data(ib, contract)
